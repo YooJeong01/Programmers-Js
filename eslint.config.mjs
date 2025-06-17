cffe2926cea5b06fed1e13f8a50adbcdc0657679
YooJeong01 <ujjh77@naver.com>
2025-06-17 13:08:30 +0900

    [Chore] eslint 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,40 +2,25 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
-const types = {
-  isObject:true,
-  isArray:true,
-  isString:true,
-  isNull:true,
-  isUndefined:true,
-}
 
-const lib = {
-  getNode:true,
-  attr:true,
-  css:true,
-  insertLast:true,
-}
 
 export default defineConfig([
-  { 
-    files: ["**/*.{js,mjs,cjs}"], 
-    plugins: { js }, 
-    extends: ["js/recommended"] 
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
   },
-  { 
-    files: ["**/*.{js,mjs,cjs}"], 
-    languageOptions: { 
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
       globals: {
-        ...globals.browser, 
+        ...globals.browser,
         ...globals.node,
-        ...types,
-        ...lib,
-        gsap:true,
-      } 
+        gsap:true
+      },
     },
-    rules: {
+    rules:{
       "no-unused-vars":"off"
-    } 
+    }
   },
 ]);
