105d54afe39371424e95d0a1320021ac15c97cae
YooJeong01 <ujjh77@naver.com>
2025-06-17 13:02:24 +0900

    [Feat] learn module

--- client/site/main.js
+++ client/site/main.js
@@ -1,63 +1,77 @@
-/* eslint-disable no-undef */
-const links = document.querySelectorAll('nav a');
-const header = document.querySelector('#header');
-const depthList = document.querySelectorAll('.depth');
-
 /* 
-1. 각 메뉴에 마우스를 올렸을 때 
-2. 선택된 자식의 depth를 찾아서 
-    - 대상의 마지막 자식 요소 (lastElementChild)
 
-3. height = '100px'
-    - element.style.height = '100px'
 
-4. 선택된 대상의 depth가 아니면 나머지는 height = 0;
-    - 나머지 = 전체
+1. input 선택하기
+    - getNode or querySelector
+
+2. input 이벤트 바인딩
+    - addEventListener('input') 
+
+3. input의 value 값 가져오기
+    - input.value
+
+4. 숫자값 더하기
+    - value1 + value2
+
+5. result에 출력하기 
+    - insertLast or insertAdjacentHTML
+
+6. clear 클릭시 모든 값 초기화
+
 
 */
 
+// import { getNode } from "./lib/dom/getNode.js";
+// import { clearContents } from './lib/dom/clearContents.js'
+// import { insertLast } from './lib/dom/insert.js'
+
+/* 
+    named export          =>     import { getNode } from '../....js'
+    default export        =>     import clearContents from '../...js'
+*/
+
+import { 
+    getNode as $, 
+    isString,
+    addClass,
+    typeError,
+    insertLast, 
+    clearContents
+} from './lib/index.js';
 
 
-function vanilla(){
-    // helper function
-    const h = t => t.style.height = 0;
 
-    links.forEach((a)=>{
-        a.addEventListener('mouseenter', (e)=>{
-            const currentDepth = e.currentTarget.lastElementChild;
-            depthList.forEach(h);
-            currentDepth.style.height = '100px';
-        })
-    })
+// import clearContents from "./lib/dom/clearContents.js";
 
-    header.addEventListener('mouseleave',()=>depthList.forEach(h));
 
+const first = $('#firstNumber');
+const second = $('#secondNumber');
+const result = $('.result');
+const clear = $('#clear');
+
+function handleInput() {
+  const firstValue = +first.value;
+  const secondValue = Number(second.value);
+  const total = firstValue + secondValue;
+
+  // result.textContent = '';
+  clearContents(result);
+
+  insertLast(result, total);
 }
 
+function handleClear(e) {
+  e.preventDefault();
 
-// console.log(gsap);
+  // first.value = ''
+  // second.value = ''
+  clearContents(first);
+  clearContents(second);
 
-// links.forEach((a)=>{
-//     const currentDepth = a.lastElementChild;
+  result.textContent = '-';
+  first.focus();
+}
 
-//     // gsap은 기본적으로 애니메이션을 바로 실행시키기때문에 멈춰놓기
-//     // transition 속성을 없애주고 gsap 넣기
-//     const tl = gsap.timeline({paused:true})
-//                 .to(currentDepth,{height:100, duration:0.5, ease:'power2.inOut'})
-
-//     a.addEventListener('mouseenter',()=>{ tl.play() })
-//     a.addEventListener('mouseleave',()=>{ tl.reverse() })
-// })
-
-
-// const tl = gsap.timeline({
-//     defaults: {
-//         duration:2,
-//     }
-// });
-
-// tl.to('.logo', { x:100, })
-// tl.to('.logo', { y:100, })
-
-// tl.restart() // 다시 시작
-// tl.reverse() // 역 재생
+first.addEventListener('input', handleInput);
+second.addEventListener('input', handleInput);
+clear.addEventListener('click', handleClear);
